d230546fb409529d3c3b360db7c5a37eb22c45fb
Dmitriy Mishchenko <ripatrip@gmail.com>
2025-03-02 16:53:55 +0200

    Configure server host and update viewport height in global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://dmmishchenko.github.io",
   integrations: [mdx(), sitemap()],
+  server: {
+    host: "0.0.0.0"
+  }
 });
